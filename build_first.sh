2fb6910a189d0551b3defbaaddc751d3d8b614e9
N1koleas <n1koleas@protonmail.com>
2026-03-09 13:36:12 +0100

    Refactor build to a bash script with incremental colcon builds and automatic environment sourcing

--- build_first.sh
+++ build_first.sh
@@ -1,15 +1,32 @@
-cd /home/ws/roarm_ws
-colcon build --packages-select roarm_msgs 
-colcon build --packages-select moveit_servo 
-colcon build --packages-select rviz_marker_tools 
-colcon build --packages-select moveit_task_constructor_msgs 
-colcon build --packages-select moveit_task_constructor_core 
-colcon build --packages-select moveit_task_constructor_capabilities 
-colcon build --packages-select moveit_task_constructor_visualization 
-colcon build --packages-select roarm_moveit_cmd 
-colcon build --packages-select roarm_moveit_ikfast_plugins 
-colcon build --packages-select roarm_moveit_mtc_demo 
-colcon build --packages-select roarm_moveit_servo 
-colcon build --packages-select roarm_description roarm_driver roarm_moveit --symlink-install 
-echo "source /home/ws/roarm_ws/install/setup.bash" >> ~/.bashrc
-source ~/.bashrc 
+#!/bin/bash
+set -e  # exit on any error
+
+WORKSPACE_DIR=$(pwd)
+
+# source existing setup if available
+if [ -f "$WORKSPACE_DIR/install/setup.bash" ]; then
+    source "$WORKSPACE_DIR/install/setup.bash"
+fi
+
+# build base package first
+colcon build --packages-select roarm_msgs --symlink-install
+
+grep -qxF "source ~/roarm_ws/install/setup.bash" ~/.bashrc || echo "source ~/roarm_ws/install/setup.bash" >> ~/.bashrc
+source "$WORKSPACE_DIR/install/setup.bash"
+
+# build packages that depend on it
+colcon build --packages-select moveit_servo rviz_marker_tools --symlink-install
+colcon build --packages-select moveit_task_constructor_msgs moveit_task_constructor_core moveit_task_constructor_capabilities moveit_task_constructor_visualization --symlink-install
+source "$WORKSPACE_DIR/install/setup.bash"
+
+# build roarm MoveIt packages
+colcon build --packages-select roarm_moveit_cmd roarm_moveit_ikfast_plugins roarm_moveit_mtc_demo roarm_moveit_servo --symlink-install
+source "$WORKSPACE_DIR/install/setup.bash"
+
+# build remaining meta packages
+colcon build --packages-select roarm_description roarm_driver roarm_moveit --symlink-install
+
+# final source so your terminal is ready
+source "$WORKSPACE_DIR/install/setup.bash"
+
+echo "Workspace built successfully!"
